b03c307842f7375433b79f2802d1d44787c2e97a
Hafiz Haseeb Ahmad Butt <hbutt.bscs19seecs@seecs.edu.pk>
2023-04-29 03:49:29 +0500

    Admin Author Status Update completed

---
+++ src/Services/AdminService/DataWriteService.js
@@ -0,0 +1,26 @@
+import {
+    ApiGatewayURL,
+    post,
+    httpStatusInternalServerError,
+    requestHeaders,
+    serviceSQLWriting
+} from "../apiConstants";
+
+export async function blockAuthor(body){
+    const response = await fetch(`${ApiGatewayURL}/${serviceSQLWriting}/blockPublisher`, {
+        method: post,
+        headers:requestHeaders,
+        body: JSON.stringify(body)
+    });
+    if (!response.ok) return httpStatusInternalServerError
+    return response.status;
+}
+export async function removeBlockAuthor(body){
+    const response = await fetch(`${ApiGatewayURL}/${serviceSQLWriting}/activePublisher`, {
+        method: post,
+        headers:requestHeaders,
+        body: JSON.stringify(body)
+    });
+    if (!response.ok) return httpStatusInternalServerError
+    return response.status;
+}
