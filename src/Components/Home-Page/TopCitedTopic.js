4e9e4dd450c6fe3d6aed3a5a84c957d4397671df
Waleed Shahid <waleedshahid524@gmail.com>
2023-04-30 00:03:17 +0500

    First version of Responsiveness

--- src/Components/Home-Page/TopCitedTopic.js
+++ src/Components/Home-Page/TopCitedTopic.js
@@ -5,7 +5,8 @@ const TopCitedTopic = () => {
         <div className={"Searched-topics"}>
             <h5 className={"heading heading-extra"}>Top Research Topics: </h5>
             <div
-            className="tags_wrapper"><a href={"#"} className={'tags'}><span >NLP</span></a>
+            className="tags_wrapper">
+            <a href={"#"} className={'tags'}><span >NLP</span></a>
             <a href={"#"} className={'tags'}><span >Biotech</span></a>
             <a href={"#"} className={'tags'}><span >NLP</span></a>
             <a href={"#"} className={'tags'}><span >Biotech</span></a>
